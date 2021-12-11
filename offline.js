fbff8016b9db0d3d1f3dc865aa55edf6f50b5eb0
diyar <68123026+Coffeepainter@users.noreply.github.com>
2021-12-11 17:51:41 +0330

    New Music player for web
    that is for my friends , thank you github

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1639232322,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"bg1.png",
+		"music_icon-sheet0.png",
+		"musical_buttons-sheet0.png",
+		"line_bar_music-sheet0.png",
+		"buttons-sheet0.png",
+		"whatsapp_icon-sheet0.png",
+		"telegram_icon-sheet0.png",
+		"bg2.png",
+		"heart_like-sheet0.png",
+		"back_button_home-sheet0.png",
+		"k.flay - high enough ( gandommusic.ir ).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
